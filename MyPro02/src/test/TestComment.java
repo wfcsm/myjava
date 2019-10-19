1a4861e874e6ecb92e311a4c5472d772ce01df33
zyc <627245048@qq.com>
2019-10-19 10:50:22 +0800

    Mypro02

---
+++ MyPro02/src/test/TestComment.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * 用来测试注释的方法（包含：多行注释，单行注释，文档注释）
+ * @author zyc
+ */
+public class TestComment {
+    public static void main(String[] args/*args是参数名 ,可以更改 */){
+        System.out.println("这是注释"); //这是打印语句...
+    }
+    /*
+    * 我是多行注释
+    * 我是多行注释
+    * 我是多行注释
+    * */
+}
